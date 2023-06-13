24a5a0205401c813258a7571618613a8a4beda7b
aleenakhan10 <aleenakhanraees40@gmail.com>
2023-06-14 03:04:26 +0500

    added methods for data view

--- panda_doc.py
+++ panda_doc.py
@@ -24,3 +24,26 @@ df2 = pd.DataFrame({
     'D': ['test', 'drill', 'drill', 'test']    
 })
 # print(df2.dtypes)
+
+
+# ______________Viewing Data______________
+
+'''we can view the data by using the following methods:
+    1- head()        => Respresents the uppers values
+    2- tail()        => Respresents the lower values
+    3- index()       => Respresents the index
+    4- column()      => Respresents the headings
+    5- to_numpy()    => Respresents the underlying data
+    5- Describe()    => Respresents the Statistical summary of the data
+we can also sort them using the sort_index() or sort_values() methods'''
+    
+df.head()
+df.tail()
+df.index
+df.columns
+df.to_numpy()
+df.describe()
+df.sort_index(axis=0, ascending=False)
+df.sort_values("B")
+
+
