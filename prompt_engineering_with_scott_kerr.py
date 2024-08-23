0f2be904b4664a949181d66b5cd317e23f0816ab
AleenaKhan10 <aleenakhanraees40@gmail.com>
2024-08-23 16:47:24 +0500

    added and new file for prompt engineering to with scott kerr.

---
+++ prompt_engineering_with_scott_kerr.py
@@ -0,0 +1,11 @@
+"""
+What is promp Engineering?
+
+prompt - instructions provided to an AI
+
+Prompt Engineering - the practice of developing and optimizing these prompts to use an AI for a certain tasks
+
+Programming in NATURAL language.
+
+
+"""
